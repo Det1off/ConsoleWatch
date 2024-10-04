e16d77b6b882fba46d81a128513f1e053c4e117b
Maxim <maxim.bakal4ul@outlook.com>
2024-10-04 23:31:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleWatch.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(0, "ru");
+
+    while (true) {
+        SYSTEMTIME now;
+        GetSystemTime(&now);
+
+        cout << "==========================================" << "\n";
+        cout << "||                                      ||" << "\n";
+        cout << "||              "
+            << setw(2) << setfill('0') << now.wDay << "."
+            << setw(2) << setfill('0') << now.wMonth << "."
+            << now.wYear << "              ||" << "\n";
+        cout << "||                                      ||" << "\n";
+        cout << "||              "
+            << setw(2) << setfill('0') << now.wHour << ":"
+            << setw(2) << setfill('0') << now.wMinute << ":"
+            << setw(2) << setfill('0') << now.wSecond << "                ||" << "\n";
+        cout << "||                                      ||" << "\n";
+        cout << "==========================================" << "\n";
+
+        Sleep(1000);
+        system("cls");
+    }
+
+    return 0;
+}
+
